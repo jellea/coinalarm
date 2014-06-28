d61415afd95d60ab2c4f06986c9628d1534646e0
TN1ck <tomwanick@gmail.com>
2014-06-28 21:05:25 +0200

    removed symbols and added currency-symbols

--- src/clj/coinalarm/btc.clj
+++ src/clj/coinalarm/btc.clj
@@ -6,47 +6,59 @@
 (def apis
   {:bitcoincharts "http://api.bitcoincharts.com/v1/markets.json"})
 
-(def symbols ["localbtcUSD" "bitxZAR" "itbitEUR" "anxhkCNY"
-              "localbtcCAD" "btcexWMZ" "ruxumTHB" "krakenLTC"
-              "bcEUR" "okcoinCNY" "rockUSD" "bitmeUSD" "btcexUSD"
-              "anxhkSGD" "anxhkAUD" "wbxAUD" "ruxumSGD" "globalUSD"
-              "ruxumAUD" "bcmBMGAU" "localbtcNOK" "freshPLN" "itbitUSD"
-              "hitbtcEUR" "1coinUSD" "weexCAD" "anxhkJPY" "localbtcZAR"
-              "weexUSD" "mtgoxTHB" "ruxumPLN" "bitbayPLN" "bcmMBUSD" "mtgoxCNY"
-              "rippleXRP" "ruxumJPY" "lybitUSD" "lybitCAD" "mtgoxAUD"
-              "bitfloorUSD" "mtgoxSGD" "bitmarketAUD" "bcmPXGAU" "localbtcINR"
-              "anxhkGBP" "hitbtcUSD" "bitboxUSD" "krakenEUR" "localbtcTHB"
-              "ruxumGBP" "britcoinGBP" "intrsngPLN" "bitmarketPLN" "mtgoxPLN"
-              "justXRP" "btcmarketsAUD" "anxhkNZD" "mtgoxJPY" "localbtcAUD"
-              "localbtcILS" "localbtcSGD" "krakenNMC" "bitaloEUR" "bitstockCZK"
-              "thLRUSD" "mtgoxGBP" "localbtcPLN" "ruxumSEK" "ruxumHUF" "bitcurexPLN"
-              "intrsngGBP" "bitmarketGBP" "krakenUSD" "bitmarketplPLN" "fybsgSGD"
-              "b7EUR" "cbxUSD" "btceEUR" "itbitSGD" "localbtcCZK" "thEUR" "rockSLL"
-              "ruxumUAH" "snwcnXRP" "btc24EUR" "bcmBMUSD" "bitaloUSD" "mtgoxNZD"
-              "localbtcGBP" "cryptoxUSD" "btcexJPY" "bbmBRL" "weexAUD" "globalPLN"
-              "justLTC" "btcdeEUR" "mtgoxSEK" "bitstampUSD" "imcexEUR" "korbitKRW"
-              "b7USD" "btceurEUR" "btcnCNY" "globalGBP" "btceUSD" "virwoxSLL"
-              "rippleEUR" "bit121GBP" "thUSD" "bit2cILS" "localbtcNZD" "ruxumRUB"
-              "btc24USD" "anxhkHKD" "anxhkCHF" "thCLP" "b2cUSD" "localbtcSEK"
-              "ruxumHKD" "bitcashCZK" "bcmLRUSD" "ruxumCHF" "anxhkEUR" "bcGBP"
-              "imcexUSD" "bitomatPLN" "zyadoEUR" "bitchangePLN" "ruxumEUR"
-              "bitfinexUSD" "mtgoxRUB" "rippleUSD" "justEUR" "localbtcBRL"
-              "krakenKRW" "bitmeLTC" "bitmarketRUB" "mrcdBRL" "lakeUSD"
-              "fbtcEUR" "btctreeUSD" "vcxEUR" "fybseSEK" "anxhkCAD"
-              "btcexYAD" "mtgoxHKD" "mtgoxCHF" "anxhkUSD" "btcexWMR" "thINR"
-              "intrsngEUR" "bitmarketEUR" "localbtcRUB" "ruxumUSD" "bitbayUSD" "rmbtbCNY"
-              "cryptoxAUD" "mtgoxEUR" "coinfloorGBP" "justUSD" "kptnSEK" "bcmPPUSD"
-              "crytrEUR" "btchkexHKD" "btcxchangeRON" "bitaloPLN" "btcexRUB" "fbtcUSD"
-              "btceRUR" "vcxUSD" "mtgoxDKK" "localbtcHKD" "localbtcCHF" "localbtcEUR"
-              "bitkonanUSD" "cotrUSD" "localbtcARS" "bitcurexEUR" "exchbUSD" "krakenXRP"
-              "thAUD" "rockEUR" "intrsngUSD" "bitmarketUSD" "mtgoxCAD" "localbtcMXN" "mtgoxUSD"
-              "btcexEUR" "bidxtrmPLN" "bitaloGBP" "btcoidIDR" "justNOK" "crytrUSD" "ruxumZAR"
-              "virtexCAD" "aqoinEUR" "localbtcDKK" "ibwtGBP" "bitnzNZD" "globalEUR"])
+(def currency-markets
+  {"CHF" ["ruxumCHF" "anxhkCHF" "localbtcCHF" "mtgoxCHF"]
+   "HKD" ["btchkexHKD" "ruxumHKD" "anxhkHKD" "localbtcHKD" "mtgoxHKD"]
+   "EUR" ["intrsngEUR" "hitbtcEUR" "btceurEUR" "zyadoEUR"
+          "itbitEUR" "ruxumEUR" "btcexEUR" "anxhkEUR" "b7EUR"
+          "fbtcEUR" "globalEUR" "imcexEUR" "btc24EUR" "btceEUR"
+          "justEUR" "bcEUR" "rockEUR" "bitcurexEUR" "mtgoxEUR" "crytrEUR"
+          "rippleEUR" "krakenEUR" "bitmarketEUR" "aqoinEUR" "thEUR" "vcxEUR"
+          "bitaloEUR" "localbtcEUR" "btcdeEUR"]
+   "ARS" ["localbtcARS"]
+   "MXN" ["localbtcMXN"]
+   "DKK" ["localbtcDKK" "mtgoxDKK"]
+   "NMC" ["krakenNMC"]
+   "USD" ["bitkonanUSD" "justUSD" "cotrUSD" "hitbtcUSD" "crytrUSD" "localbtcUSD" "rippleUSD"
+          "itbitUSD" "1coinUSD" "bitbayUSD" "bitboxUSD" "b7USD" "bitmeUSD" "cryptoxUSD" "bitfloorUSD"
+          "btc24USD" "btcexUSD" "btcexWMZ" "btctreeUSD" "anxhkUSD" "thUSD" "bitstampUSD" "bcmBMUSD"
+          "bcmLRUSD" "bcmMBUSD" "b2cUSD" "exchbUSD" "fbtcUSD" "globalUSD" "imcexUSD" "ruxumUSD" "thLRUSD"
+          "cbxUSD" "krakenUSD" "bitfinexUSD" "lakeUSD" "rockUSD" "vcxUSD" "bitaloUSD" "mtgoxUSD"
+          "bcmPPUSD" "lybitUSD" "btceUSD" "bitmarketUSD" "intrsngUSD" "weexUSD"]
+   "CAD" ["lybitCAD" "mtgoxCAD" "anxhkCAD" "virtexCAD" "weexCAD" "localbtcCAD"]
+   "CLP" ["thCLP"], "NOK" ["localbtcNOK" "justNOK"]
+   "ZAR" ["bitxZAR" "ruxumZAR" "localbtcZAR"]
+   "INR" ["localbtcINR" "thINR"]
+   "THB" ["ruxumTHB" "localbtcTHB" "mtgoxTHB"]
+   "CNY" ["btcnCNY" "okcoinCNY" "mtgoxCNY" "rmbtbCNY" "anxhkCNY"]
+   "SGD" ["anxhkSGD" "itbitSGD" "ruxumSGD" "fybsgSGD" "localbtcSGD" "mtgoxSGD"]
+   "AUD" ["cryptoxAUD" "ruxumAUD" "anxhkAUD" "mtgoxAUD" "btcmarketsAUD" "thAUD" "weexAUD" "bitmarketAUD" "localbtcAUD" "wbxAUD"]
+   "ILS" ["bit2cILS" "localbtcILS"], "KRW" ["krakenKRW" "korbitKRW"]
+   "RON" ["btcxchangeRON"]
+   "PLN" ["mtgoxPLN" "bitbayPLN" "bidxtrmPLN" "localbtcPLN" "bitomatPLN" "freshPLN" "globalPLN" "ruxumPLN" "intrsngPLN" "bitaloPLN" "bitmarketplPLN" "bitchangePLN" "bitmarketPLN" "bitcurexPLN"]
+   "SLL" ["rockSLL" "virwoxSLL"]
+   "JPY" ["btcexJPY" "anxhkJPY" "ruxumJPY" "mtgoxJPY"]
+   "CZK" ["localbtcCZK" "bitcashCZK" "bitstockCZK"]
+   "GBP" ["ruxumGBP" "britcoinGBP" "ibwtGBP" "coinfloorGBP" "anxhkGBP" "globalGBP"
+          "localbtcGBP" "bitmarketGBP" "bitaloGBP" "bit121GBP" "bcGBP" "mtgoxGBP" "intrsngGBP"]
+   "XRP" ["krakenXRP" "justXRP" "rippleXRP" "snwcnXRP"]
+   "IDR" ["btcoidIDR"]
+   "NZD" ["anxhkNZD" "mtgoxNZD" "bitnzNZD" "localbtcNZD"]
+   "SEK" ["localbtcSEK" "ruxumSEK" "fybseSEK" "kptnSEK" "mtgoxSEK"]
+   "HUF" ["ruxumHUF"], "GAU" ["bcmPXGAU" "bcmBMGAU"]
+   "LTC" ["bitmeLTC" "justLTC" "krakenLTC"]
+   "BRL" ["bbmBRL" "mrcdBRL" "localbtcBRL"]
+   "UAH" ["ruxumUAH"]
+   "RUB" ["btcexRUB" "btcexWMR" "btcexYAD" "localbtcRUB" "ruxumRUB" "btceRUR" "bitmarketRUB" "mtgoxRUB"]})
 
 (defn get-latest-data [{:keys [api query-params callback]}]
   (let [url (or (api apis) (:bitcoincharts apis))]
     (client/get url {:query-params query-params}
                 callback)))
+
+(defn get-currency-markets []
+  (into {} (map (fn [[f s]] (vector f (mapv :symbol s)))
+                (group-by :currency (json->dict (:body (deref (client/get (:bitcoincharts apis)))))))))
 
 (defn json->dict [data]
   (json/read-str data :key-fn keyword))
