e40c9cc06237df3815cb6ff1c0a18387ea9ed6f4
TN1ck <tomwanick@gmail.com>
2014-06-28 21:40:19 +0200

    started integrating sms

--- src/clj/coinalarm/communication.clj
+++ src/clj/coinalarm/communication.clj
@@ -1,4 +1,13 @@
-(ns coinalarm.communication)
+(ns coinalarm.communication
+  (:require [org.httpkit.client :as client]
+            [clojure.data.json :as json]))
 
-(defn send-sms [phone text]
-  (print phone text))
+(def auth-keys {:AUTH_ID (or (System/getenv "PLIVO_AUTH_ID") "woop")
+                :AUTH_TOKEN (or (System/getenv "PLIVO_AUTH_TOKEN") "voop")})
+
+(def url (str "https://api.plivo.com/v1/Account/" (:AUTH_ID auth-keys) "/Message/"))
+
+(defn send-sms [{:keys [phone text callback]}]
+  (client/post url {:query-params {:dst phone :text text :src 0000 :type "sms"}} callback))
+
+
