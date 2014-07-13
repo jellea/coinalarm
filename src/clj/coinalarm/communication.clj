67418854edb2a0f1799571aa569b38a367795149
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-07-13 16:32:35 +0200

    sending text messages now works

--- src/clj/coinalarm/communication.clj
+++ src/clj/coinalarm/communication.clj
@@ -2,10 +2,13 @@
   (:require [org.httpkit.client :as client]
             [clojure.data.json :as json]))
 
-(def auth-keys {:AUTH_ID (or (System/getenv "PLIVO_AUTH_ID") "woop")
-                :AUTH_TOKEN (or (System/getenv "PLIVO_AUTH_TOKEN") "voop")})
+(def auth-keys {:AUTH_ID (or (System/getenv "PLIVO_AUTH_ID") "")
+                :AUTH_TOKEN (or (System/getenv "PLIVO_AUTH_TOKEN") "")
+                :NUMBER (or (System/getenv "PLIVO_NUMBER") "")})
 
 (def url (str "https://api.plivo.com/v1/Account/" (:AUTH_ID auth-keys) "/Message/"))
 
-(defn send-sms [{:keys [phone text callback]}]
-  (client/post url {:query-params {:dst phone :text text :src 0000 :type "sms"}} callback))
+(defn send-sms [phone text]
+  (client/post url {:basic-auth [(:AUTH_ID auth-keys) (:AUTH_TOKEN auth-keys)]
+                    :headers {"Content-Type" "application/json"}
+                    :body (json/write-str {"dst" phone "text" text "src" (:NUMBER auth-keys) "type" "sms"})}))
